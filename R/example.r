b5da78f83a0413ce1fe173179cc93061d9f9f1f4
MLMecham <mec21017@byui.edu>
2026-01-13 16:28:07 -0700

    built the docs files for the website

--- R/example.r
+++ R/example.r
@@ -5,3 +5,11 @@
 add2 <- function(x1, x2) {
   x1 + x2
 }
+
+#' An example multiplication function
+#' @param x1 is the first number in the multiplication
+#' @param x2 is the second number in the multiplication
+#' @export 
+multiply2 <- function(x1, x2) {
+  x1 * x2
+}
